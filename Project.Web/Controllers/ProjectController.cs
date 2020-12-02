3f4d3b2b5e5025e73f269545f87cab2d9a61972d
Neil Busse <nbuss848@gmail.com>
2020-12-01 20:23:29 -0600

    Basic Auto Mapper Setup

--- Project.Web/Controllers/ProjectController.cs
+++ Project.Web/Controllers/ProjectController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Project.Infrastructure.Persistence;
 using Project.Web.Models;
@@ -11,18 +12,28 @@ namespace Project.Web.Controllers
     public class ProjectController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
 
-        public ProjectController(ApplicationDbContext context)
+        public ProjectController(ApplicationDbContext context, IMapper automapper)
         {
             _context = context;
+            _mapper = automapper;
         }
 
         public IActionResult Index()
         {
-            var view = _context.Projects.Select(x => new ProjectViewModel() { Name = x.Name, Description = x.Description });
+            var view = _mapper.ProjectTo<ProjectViewModel>(_context.Projects);
             var modelview = new ProjectIndexViewModel() { ProjectViewModels = view };
-
+            
             return View(modelview);
+        }
+    }
+
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Domain.Entities.Project, ProjectViewModel>();
         }
     }
 }
--- Project.Web/Startup.cs
+++ Project.Web/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,9 @@ namespace Project.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddAutoMapper(typeof(Startup));
+            
 
             services.AddDbContext<ApplicationDbContext>(opts => {
                 opts.EnableDetailedErrors();
