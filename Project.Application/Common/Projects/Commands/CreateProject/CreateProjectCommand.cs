c1569f3fab3b3ca3cc96cfe7d23e0a63d173d68c
Neil <nbuss848@gmail.com>
2020-12-05 14:50:00 -0600

    Create project page (#2)
    * Basic MediatR setup
    
    * Without mediatoR setup

---
+++ Project.Application/Common/Projects/Commands/CreateProject/CreateProjectCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Application.Common.Projects.Commands.CreateProject
+{
+    public class CreateProjectCommand
+    {
+
+    }
+}
--- Project.Web/Controllers/ProjectController.cs
+++ Project.Web/Controllers/ProjectController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Project.Infrastructure.Persistence;
 using Project.Web.Models;
 
@@ -24,8 +25,44 @@ namespace Project.Web.Controllers
         {
             var view = _mapper.ProjectTo<ProjectViewModel>(_context.Projects);
             var modelview = new ProjectIndexViewModel() { ProjectViewModels = view };
+           
             
             return View(modelview);
+        }
+
+        public IActionResult Create()
+        {
+            var status = new SelectList(_context.Statuses.OrderBy(x => x.StatusId).Select(x=>x.Name));
+            
+            var createView = new ProjectCreateModel() 
+            { 
+                StatusList = status, 
+                CreatedDate=DateTime.Now 
+            };
+
+            return View(createView);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProject(ProjectCreateModel model)
+        {
+            var project = new Domain.Entities.Project()
+            {
+                ProjectId = 0,
+                CreatedDate = model.CreatedDate,
+                Description = model.Description,
+                Name = model.Name,
+                Size = model.Size,
+                Priority = model.Priority,
+                Classification = model.Classification,
+                DueDate = model.DueDate,
+                ProjectImage = model.ProjectImage
+            };
+
+            await _context.AddAsync(project);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Project");
         }
     }
 
@@ -33,7 +70,7 @@ namespace Project.Web.Controllers
     {
         public MappingProfile()
         {
-            CreateMap<Domain.Entities.Project, ProjectViewModel>();
+            CreateMap<Domain.Entities.Project, ProjectViewModel>();            
         }
     }
 }
