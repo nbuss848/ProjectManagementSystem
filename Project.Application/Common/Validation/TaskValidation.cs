1faf9f4e3d534b8b110b989a712b4711b748d289
Neil <nbuss848@gmail.com>
2021-01-10 21:30:43 -0600

    Task size must be more than zero

---
+++ Project.Application/Common/Validation/TaskValidation.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Application.Common.Validation
+{
+    public class TaskValidation : AbstractValidator<Project.Domain.Entities.Task>
+    {
+        public TaskValidation()
+        {
+            RuleFor(x => x.Size).GreaterThan(0);
+        }
+    }
+}
