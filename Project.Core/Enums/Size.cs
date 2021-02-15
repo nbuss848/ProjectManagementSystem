8dacc138ce5228c354afa634bca3df0df9d00ead
Neil <nbuss848@gmail.com>
2021-02-14 20:21:45 -0600

    Add default size

---
+++ Project.Core/Enums/Size.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Domain.Enums
+{
+    public enum Size
+    {
+        Mini = 1,
+        Small = 2,
+        Medium = 4,
+        Large = 8
+    }
+}
