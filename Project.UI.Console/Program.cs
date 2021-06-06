07f713ec5a18bc2f9bf5c37ec6a6acb16cad4cb9
Neil Busse <nbuss848@gmail.com>
2021-06-05 23:22:02 -0500

    Clean up program in console app

--- Project.UI.Console/Program.cs
+++ Project.UI.Console/Program.cs
@@ -21,29 +21,7 @@ namespace Project.UI.CW
         static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-            //var options = host.Services.GetRequiredService<IMediator>().Value;
-            //var host = CreateHostBuilder(args).Build();
-            //host.Services.GetRequiredService<Program>.
-
-            //var builder = new ConfigurationBuilder()
-            //    .SetBasePath(Directory.GetCurrentDirectory())
-            //    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-
-            //IConfiguration config = builder.Build();
-
-            //var host = IHostConfiguration(config);
-
-            //host.Services.GetRequiredService<Program>().GetProjects();
         }
-
-        //private void GetProjects()
-        //{
-        //    var projects = _mediator.Send(new GetProjectsQuery());
-        //    foreach (var project in projects.Result.ProjectViewModels)
-        //    {
-        //        Console.WriteLine(project.Description + " " + project.NumberOfTasks);
-        //    }
-        //}
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
        Host.CreateDefaultBuilder(args)
@@ -66,28 +44,5 @@ namespace Project.UI.CW
                services.AddScoped<ICurrentUserService, IdentityService>();
                services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
            });
-
-        private static IHost IHostConfiguration(IConfiguration configuration)
-        {
-            var builder = new HostBuilder()
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddTransient<Program>();
-                    services.AddMediatR(typeof(Application.Common.Queries.CreateProjectQuery).GetTypeInfo().Assembly);
-                    services.AddAutoMapper(Assembly.GetExecutingAssembly());
-                    //services.AddLogging(configure => configure.AddConsole());
-                    services.AddDbContext<ApplicationDbContext>(opts =>
-                    {
-                        opts.EnableDetailedErrors();
-                        opts.UseNpgsql(configuration.GetConnectionString("Default"));
-                    });
-                    services.AddScoped<ICurrentUserService, IdentityService>();
-                    services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
-                });
-
-            var host = builder.Build();
-
-            return host;      
-        }
     }
 }
